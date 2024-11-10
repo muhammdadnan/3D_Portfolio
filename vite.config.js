908157498bf09b5a0b6e6a3c0dfb2925c3acbade
Muhammad Adnan <adnan892009@gmail.com>
2024-11-10 22:56:09 +0500

    vite config Change done

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
-})
+  assetsInclude: ["**/*.glb"],
+});
